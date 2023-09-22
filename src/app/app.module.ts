82edf8adb4e53b2c72d125500db97af88a29a163
Nikhil_Prabhat <nikhilprabhat03051999@gmail.com>
2023-09-22 16:34:13 +0530

    HM-UI :Login Done & Signup Created & Routing Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,25 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { SecurityComponent } from './security/security.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { SecurityServiceService } from './service/security-service.service';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SecurityComponent
+    SecurityComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [SecurityServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/security/security.component.ts
+++ src/app/security/security.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { SecurityServiceService } from '../service/security-service.service';
 import { AuthRequest } from '../modals/security/AuthRequest.modal';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-security',
@@ -9,27 +11,48 @@ import { AuthRequest } from '../modals/security/AuthRequest.modal';
 })
 export class SecurityComponent implements OnInit {
 
-  authRequest: AuthRequest = {
-    "username": "Honey",
-    "password": "Honey@123"
-  };
+  LOGIN_UNSUCCESSFUL_MSG = "Login Unsuccessful, Plz try again !";
+  LOGIN_SUCCESSFUL_MSG = "Login Successful, Welcome ";
+
+  @ViewChild('loginForm') loginForm!: NgForm;
 
   tokenValid: boolean = false;
-  
-  constructor(private securityService: SecurityServiceService) { }
+  public loginMessage!: string;
+  public isSubmitted !: boolean;
+  public isLoginSuccess !: boolean;
+
+  constructor(private securityService: SecurityServiceService, private router: Router) { }
 
   ngOnInit(): void {
-
+    this.isSubmitted = false;
+    this.isLoginSuccess = false;
   }
 
   /* Generate Token */
-  public loginAndCreateToken(authRequest: AuthRequest) {
+  public loginAndCreateToken() {
+    console.log(this.loginForm);
+    this.isSubmitted = true;
+
+    // Capturing the data from the form
+    var username = this.loginForm.value.username;
+    var password = this.loginForm.value.password;
+    var authRequest = new AuthRequest(username, password);
+
     this.securityService.loginAndCreateToken(authRequest)
       .subscribe(
         tokenWithUser => {
+          // In case of success response
           this.validate(tokenWithUser.token);
+          this.isLoginSuccess = true;
+          this.loginMessage = this.LOGIN_SUCCESSFUL_MSG + tokenWithUser.username + " !";
+          this.routeToSignupPage(tokenWithUser.username);
+        }, (error: any) => {
+          this.isLoginSuccess = false;
+          this.loginMessage = this.LOGIN_UNSUCCESSFUL_MSG;
         }
       );
+
+    this.loginForm.reset();
   }
 
   /* Validate Token */
@@ -38,6 +61,14 @@ export class SecurityComponent implements OnInit {
       .subscribe(
         tokenValidationResponse => this.tokenValid = tokenValidationResponse.isValid
       );
+  }
+
+  /* Route to Signup Page */
+  public routeToSignupPage(username: string) {
+    setTimeout(() => {
+      // For now, it is randomly navigated to signup page
+      this.router.navigate(['/signup', username])
+    }, 2000);
   }
 
 

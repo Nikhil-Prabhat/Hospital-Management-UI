ca7543671873b439dca3828d3c662c5f5c7b7c13
Nikhil_Prabhat <nikhilprabhat03051999@gmail.com>
2023-10-03 20:53:15 +0530

    HM-UI : Treatment History Endpoints Done

--- src/app/treatment-history/savetreatmenthistory/savetreatmenthistory.component.ts
+++ src/app/treatment-history/savetreatmenthistory/savetreatmenthistory.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DoctorResponse } from 'src/app/modals/hospital/DoctorResponse.modal';
+import { PatientResponse } from 'src/app/modals/hospital/PatientResponse.modal';
+import { TreatmentHistory } from 'src/app/modals/hospital/TreatmentHistory.modal';
+import { TreatmentHistoryResponse } from 'src/app/modals/hospital/TreatmentHistoryResponse.modal';
 import { HospitalService } from 'src/app/service/hospital.service';
 
 @Component({
@@ -12,12 +16,17 @@ export class SavetreatmenthistoryComponent implements OnInit {
 
   SAVE_TREATMENT_HISTORY_FAILURE = "Save Treatment History Operation Failure ! ";
 
+  patientList: PatientResponse[] = [];
+  doctorList: DoctorResponse[] = [];
+
   @ViewChild('saveTreatmentHistoryForm') saveTreatmentHistoryForm!: NgForm;
 
   saveTreatmentHistoryMessage!: string;
   isSubmitted !: boolean;
   isSaveTreatmentHistorySuccess !: boolean;
   token !: string;
+  loadPatientMessage: string = "";
+  loadDoctorMessage: string = "";
 
   constructor(private hospitalService: HospitalService, private activatedRoute: ActivatedRoute, private router: Router) { }
 
@@ -25,11 +34,45 @@ export class SavetreatmenthistoryComponent implements OnInit {
     this.token = this.activatedRoute.snapshot.params['token'];
     this.isSubmitted = false;
     this.isSaveTreatmentHistorySuccess = false;
+
+    this.loadDoctorMessage = "";
+    this.loadPatientMessage = "";
+
+    /* Initialise doctor and patient list */
+    this.getAllDoctors();
+    this.getAllPatients();
   }
 
   /* Save Treatment History */
   public saveTreatmentHistory() {
     console.log(this.saveTreatmentHistoryForm);
+    this.isSubmitted = true;
+
+    // Capturing the data from the form
+    var patientIdFromName = this.saveTreatmentHistoryForm.value.patientName;
+    var doctorIdFromName = this.saveTreatmentHistoryForm.value.doctorName;
+    var briefDescription = this.saveTreatmentHistoryForm.value.briefDescription;
+    var symptoms = this.saveTreatmentHistoryForm.value.symptoms;
+    var treatment = this.saveTreatmentHistoryForm.value.treatment;
+
+    var doctor = new DoctorResponse(doctorIdFromName, "", "", "", "");
+    var patient = new PatientResponse(patientIdFromName, "", "", "", "", "NOT_STARTED");
+    var treatmentHistory = new TreatmentHistory(patient, doctor, briefDescription, symptoms, treatment);
+
+    this.hospitalService.saveTreatmentHistory(this.token, treatmentHistory)
+      .subscribe(
+        successResponse => {
+          this.isSaveTreatmentHistorySuccess = true;
+          this.saveTreatmentHistoryMessage = JSON.stringify(successResponse);
+        }, (error: any) => {
+          this.isSaveTreatmentHistorySuccess = false;
+          this.saveTreatmentHistoryMessage = this.SAVE_TREATMENT_HISTORY_FAILURE + JSON.stringify(error.error);
+        }
+      );
+
+      this.routeToTreatmentHistoryPage(this.token);
+      this.saveTreatmentHistoryForm.reset();
+
   }
 
   /* Route to Treatment History Page */
@@ -37,6 +80,34 @@ export class SavetreatmenthistoryComponent implements OnInit {
     setTimeout(() => {
       this.router.navigate(['/treatmenthistories', token])
     }, 2000);
+  }
+
+  /* Get all the patient list */
+  public getAllPatients() {
+    this.hospitalService.getAllPatients(this.token)
+      .subscribe(
+        (patientList: PatientResponse[]) => {
+          for (var patient of patientList) {
+            this.patientList.push(patient);
+          }
+        }, (error: any) => {
+          this.loadPatientMessage = JSON.stringify(error.error);
+        }
+      );
+  }
+
+  /* Get All Doctor's List */
+  public getAllDoctors() {
+    this.hospitalService.getAllDoctors(this.token)
+      .subscribe(
+        (doctorList: DoctorResponse[]) => {
+          for (var doctor of doctorList) {
+            this.doctorList.push(doctor);
+          }
+        }, (error: any) => {
+          this.loadDoctorMessage = JSON.stringify(error.error);
+        }
+      )
   }
 
 }
